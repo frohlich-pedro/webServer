a5d0f3af8d9c41f690022ecd4359b5e2a517b4bb
frohlich-pedro <pedro_h_nf@outlook.com>
2025-02-19 22:14:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,64 +4,16 @@ import java.net.Socket;
 
 public class Main {
     private static final short PORT = 3030;
-    private static final String PATH_TO_HTML = "index.html";
+    private static final String PATH_TO_HTML = "C:\\Users\\pedro\\IdeaProjects\\untitled-1\\src\\index.html";
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
-                try (Socket clientSocket = serverSocket.accept()) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    OutputStream out = clientSocket.getOutputStream();
-
-                    String requestLine = in.readLine();
-                    if (requestLine != null) {
-                        System.out.println("Request received: " + requestLine);
-
-                        String headerLine;
-                        while (!(headerLine = in.readLine()).isEmpty()) {
-                            System.out.println(headerLine);
-                        }
-
-                        String[] requestParts = requestLine.split(" ");
-                        String method = requestParts[0];
-                        String path = requestParts[1];
-
-                        if (method.equals("GET") && path.equals("/")) {
-                            StringBuilder contentBuilder = new StringBuilder();
-                            try (BufferedReader br = new BufferedReader(new FileReader(PATH_TO_HTML))) {
-                                String currentLine;
-                                while ((currentLine = br.readLine()) != null) {
-                                    contentBuilder.append(currentLine).append("\n");
-                                }
-                            } catch (IOException e) {
-                                System.err.println(e.getMessage());
-                            }
-
-                            String htmlContent = contentBuilder.toString();
-                            String response = """
-                                    HTTP/1.1 200 OK\r
-                                    Content-Type: text/html\r
-                                    Connection: close\r
-                                    \r
-                                    """ +
-                                    htmlContent;
-
-                            out.write(response.getBytes());
-                            out.flush();
-                        } else {
-                            String response = """
-                                    HTTP/1.1 404 Not Found\r
-                                    Content-Type: text/html\r
-                                    Connection: close\r
-                                    \r
-                                    <html>404 not found</html>
-                                    """;
-
-                            out.write(response.getBytes());
-                            out.flush();
-                        }
-                    }
-
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    ClientHandler clientHandler = new ClientHandler(clientSocket, PATH_TO_HTML);
+                    Thread thread = new Thread(clientHandler);
+                    thread.start();
                 } catch (IOException e) {
                     System.err.println(e.getMessage());
                 }
